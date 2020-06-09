3502b3b1e0282520fa687b00327ff549a48b9fb2
dipluridae <40880844+dipluridae@users.noreply.github.com>
2020-06-09 22:53:13 +1000

    Add files via upload

--- main/java/com/example/lprecordlibrary/MainActivity.java
+++ main/java/com/example/lprecordlibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lprecordlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,31 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText userEditText = findViewById(R.id.userEditText);
-        final EditText passwordEditText = findViewById(R.id.password1EditText);
-        Button loginButton = findViewById(R.id.userLoginButton);
-        Button signupButton = findViewById(R.id.registerButton);
-        db= new DatabaseHelper(this);
+
+        final EditText nameEditText = findViewById(R.id.nameEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.register1Button);
+        db = new DatabaseHelper(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                boolean result = db.fetchUser(userEditText.getText().toString(), passwordEditText.getText().toString());
-                if (result == true)
+                boolean result = db.fetchUser(nameEditText.getText().toString(), passwordEditText.getText().toString());
+                if (result)
                 {
-                    Toast.makeText(MainActivity.this, "Successfully logged in!", Toast.LENGTH_LONG).show();
-
+                    Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_LONG).show();
                 }
                 else
-                {
-                    Toast.makeText(MainActivity.this, "THe user does not exist!", Toast.LENGTH_LONG).show();
+                    {
+                        Toast.makeText(MainActivity.this, "The user does nor exist", Toast.LENGTH_LONG).show();
                 }
 
-            }
+                }
         });
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
--- main/java/com/example/lprecordlibrary/RegisterActivity.java
+++ main/java/com/example/lprecordlibrary/RegisterActivity.java
@@ -1,67 +1,46 @@
 package com.example.lprecordlibrary;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class RegisterActivity extends AppCompatActivity {
-
     DatabaseHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        final EditText userEditText = findViewById(R.id.userEditText);
+        final EditText nameRegisterEditText = findViewById(R.id.nameRegisterEditText);
         final EditText password1EditText = findViewById(R.id.password1EditText);
         final EditText password2EditText = findViewById(R.id.password2EditText);
-        Button registerButton = findViewById(R.id.registerButton);
+        Button register2Button = findViewById(R.id.register2Button);
         db = new DatabaseHelper(this);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        register2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user = userEditText.getText().toString().trim();
-                String pwd = password1EditText.getText().toString().trim();
-                String confirmed_pwd = password2EditText.getText().toString().trim();
-                if (pwd.equals(confirmed_pwd))
+                String user = nameRegisterEditText.getText().toString().trim();
+                String pwd1 = password1EditText.getText().toString().trim();
+                String pwd2 = password2EditText.getText().toString().trim();
+                if (pwd1.equals(pwd2))
                 {
-                    long result = db.insert(user, pwd);
-                    if (result >0)
+                    long result = db.insert(user, pwd1);
+                    if (result > 0)
                     {
-                        Toast.makeText(RegisterActivity.this, "Registered successfully!", Toast.LENGTH_LONG).show();
-                        Intent backIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                        startActivity(backIntent);
+                        Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_LONG).show();
                     }
                     else
-                    {Toast.makeText(RegisterActivity.this, "Registration error!", Toast.LENGTH_LONG).show();}
-
+                    {
+                        Toast.makeText(RegisterActivity.this, "The passwords do not match", Toast.LENGTH_LONG).show();
+                    }
                 }
-                else
-                {
-                    Toast.makeText(RegisterActivity.this, "Two passwords do not match!!", Toast.LENGTH_LONG).show();
-                }
-
 
             }
         });
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
